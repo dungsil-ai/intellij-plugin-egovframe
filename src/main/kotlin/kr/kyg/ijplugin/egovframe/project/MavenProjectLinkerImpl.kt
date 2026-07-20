c2dd269547f1c5fd0aeb0f81f64fa26181ad0eb7
☁️dungsil <iam@younggeon.kim>
2026-07-20 21:26:43 +0900

    Maven 프로젝트 로드를 동기화하도록 개선

--- src/main/kotlin/kr/kyg/ijplugin/egovframe/project/MavenProjectLinkerImpl.kt
+++ src/main/kotlin/kr/kyg/ijplugin/egovframe/project/MavenProjectLinkerImpl.kt
@@ -2,12 +2,15 @@ package kr.kyg.ijplugin.egovframe.project
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
+import org.jetbrains.idea.maven.buildtool.MavenSyncSpec
 import org.jetbrains.idea.maven.project.MavenProjectsManager
 import java.nio.file.Path
 
 class MavenProjectLinkerImpl : MavenProjectLinker {
   override fun link(project: Project, pomPath: Path) {
     val pomFile = LocalFileSystem.getInstance().refreshAndFindFileByNioFile(pomPath) ?: return
-    MavenProjectsManager.getInstance(project).addManagedFiles(listOf(pomFile))
+    val manager = MavenProjectsManager.getInstance(project)
+    manager.addManagedFiles(listOf(pomFile))
+    manager.scheduleUpdateAllMavenProjects(MavenSyncSpec.full("eGovFrame 템플릿 프로젝트 생성"))
   }
 }
